0571372818e46565aafbb63769f73d98103e5d85
LordeSupremo <jho.chapeco@gmail.com>
2020-03-31 16:13:42 -0300

    Adicionado cabecalho de Authorization no header das requisicoes do Swagger

--- transferencias_compacito/src/main/java/br/com/unoesc/transferenciacompacito/swagger/configuracao/SwaggerConfiguration.java
+++ transferencias_compacito/src/main/java/br/com/unoesc/transferenciacompacito/swagger/configuracao/SwaggerConfiguration.java
@@ -2,12 +2,14 @@ package br.com.unoesc.transferenciacompacito.swagger.configuracao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.*;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -15,14 +17,21 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.unoesc.transferenciacompacito"))
-                .build()
-                .apiInfo(getApiInfo());
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("br.com.unoesc.transferenciacompacito"))
+            .build()
+            .apiInfo(getApiInfo())
+            .ignoredParameterTypes(ResponseEntity.class)
+            .globalOperationParameters(Arrays.asList(new ParameterBuilder()
+                .name("Authorization")
+                .description("Header for token JWT")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false).build()
+            ));
     }
 
     private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("Transferencia Compacito API").build();
+        return new ApiInfoBuilder().title("Transferencia Compacito API").build();
     }
 }
